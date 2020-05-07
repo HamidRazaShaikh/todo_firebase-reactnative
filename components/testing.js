eafb5a94853ea114de987968c98c42ea5aad01d4
unknown <hamidraza.chemistry@gmail.com>
2020-05-07 08:43:03 -0700

    third commit

--- components/testing.js
+++ components/testing.js
@@ -13,42 +13,81 @@ import {
   ScrollView,
   StatusBar,
   Alert,
+  SnapshotViewIOS,
 } from 'react-native';
 
 export default function Testing({route}) {
   const [todo, settodo] = useState('');
   const [todoData, settodoData] = useState([]);
+  const [Edit, setEdit] = useState(false);
+  const [id, setid] = useState('');
   const {user} = route.params;
+  const ref = firestore().collection(JSON.stringify(user));
 
   useEffect(() => {
-    firestore()
-      .collection(JSON.stringify(user))
-      .onSnapshot(querySnapshot => {
-        const todo = [];
-        querySnapshot.forEach(doc => {
-          todo.push({
-            todo: doc.data().todo.todo,
-            key: doc.id,
-          });
+    return ref.onSnapshot(querySnapshot => {
+      const todo = [];
+      querySnapshot.forEach(doc => {
+        todo.push({
+          todo: doc.data().todo.todo,
+          key: doc.id,
+          time: doc.data().time,
         });
-        
-        settodoData(todo)
-
-
       });
+
+      settodoData(todo);
+    });
   });
 
-  function handleAddTodo() {
-    firestore()
-      .collection(JSON.stringify(user))
-      .add({
-        todo: todo,
-      })
-      .then(() => {
-        console.log('document written');
-      });
-    // console.log(todoData);
+  async function handleAddTodo() {
+    if (Edit === false && todo !== '') {
+      await ref
+        .add({
+          todo: todo,
+          time: firestore.FieldValue.serverTimestamp(),
+        })
+        .then(() => {
+          console.log('document written');
+        });
+      settodo('');
+      // console.log(todoData);
+    } else if ( Edit === true && todo !== '') {
+      await ref
+        .doc(id)
+        .update({
+          todo: todo,
+          time: firestore.FieldValue.serverTimestamp(),
+        })
+        .then(() => {
+          console.log('document written');
+        });
+      settodo('');
+      setEdit(false);
+      settodoData([]);
+    }
+
+    else  {
+       Alert.alert('Oops!?', 'Please enter some todo...')
+
+    
   }
+  }
+  opacityTouch = item => {
+    return Alert.alert('Choose what to do?', 'Please select an option.', [
+      {
+        text: 'Edit',
+        onPress: () => setEdit(true) & settodo(item.todo) & setid(item.key),
+      },
+      {
+        text: 'Delete',
+        onPress: () =>
+          ref
+            .doc(item.key)
+            .delete()
+            .then(() => console.log('deleted')),
+      },
+    ]);
+  };
 
   return (
     <View>
@@ -60,18 +99,22 @@ export default function Testing({route}) {
       />
 
       <View>
-        <Button title={'add todo'} color="#e93766" onPress={handleAddTodo} />
+        <Button
+          title={Edit ? 'Update' : 'add todo'}
+          color="#e93766"
+          onPress={handleAddTodo}
+        />
 
         <FlatList
           data={todoData}
           renderItem={({item}) => (
             <TouchableOpacity
               style={styles.opacity}
-              onPress={() => this.opacityTouch(item)}>
+              onPress={() => opacityTouch(item)}>
               <Text style={styles.listText}>{item.todo}</Text>
             </TouchableOpacity>
           )}
-          keyExtractor={item => item.id}
+          keyExtractor={item => item.key}
         />
       </View>
     </View>
@@ -97,5 +140,10 @@ const styles = StyleSheet.create({
   opacity: {
     borderBottomWidth: 1,
     margin: 3,
+  },
+  timeText: {
+    fontSize: 10,
+    color: 'red',
+    padding: 10,
   },
 });
